59a9449bc6d2268a3fed5f10d80ddbda7501098a
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-06-01 14:58:45 +0530

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+
+function buildStyles(){
+    return src('index.scss')
+    .pipe(sass())
+    .pipe(dest('css'))
+}
+
+function watchTask() {
+    watch(['indedx.scss'], buildStyles)
+} 
+
+exports.default = series(buildStyles, watchTask)
