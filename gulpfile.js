446c486269ea7ebab2975a76a5e9750eaa502eae
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-06-01 15:39:58 +0530

    variables, partials & imports, project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const {src, dest, watch, series} = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
 
-function buildStyles(){
-    return src('index.scss')
+function buildStyles() {
+  return src('*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['indedx.scss'], buildStyles)
-} 
+  watch(['*.scss'], buildStyles)
+}
 
 exports.default = series(buildStyles, watchTask)
